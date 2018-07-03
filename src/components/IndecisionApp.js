948e4e6fde9291e1e08cb82fca9cf00c4cf1921d
Florent Nuttens <fnuttens@gmail.com>
2018-07-03 23:21:50 +0200

    Add scss variables file and style

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -77,15 +77,17 @@ export default class IndecisionApp extends React.Component {
     return (
       <div>
         <Header subtitle="Put your life in the hands of a computer" />
-        <Action
-          hasOptions={this.state.options.length > 0}
-          handlePick={this.handlePick} />
-        <Options
-          options={this.state.options}
-          handleDeleteOptions={this.handleDeleteOptions}
-          handleDeleteOption={this.handleDeleteOption} />
-        <AddOption
-          handleAddOption={this.handleAddOption} />
+        <div className="container">
+          <Action
+            hasOptions={this.state.options.length > 0}
+            handlePick={this.handlePick} />
+          <Options
+            options={this.state.options}
+            handleDeleteOptions={this.handleDeleteOptions}
+            handleDeleteOption={this.handleDeleteOption} />
+          <AddOption
+            handleAddOption={this.handleAddOption} />
+        </div>
         <OptionModal
           selectedOption={this.state.selectedOption}
           handleClose={this.handleClearSelectedOption} />
